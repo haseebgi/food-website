42d9553ef3cafba3b3710a07e1d2282552ca508d
Abdul Haseeb <abdulhaseeb0085@gmail.com>
2026-07-23 17:30:13 +0500

    Add deployment setup

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -9,7 +9,7 @@
 <link href="https://fonts.googleapis.com/css2?family=Anton&family=Inter:wght@400;500;600;700&family=JetBrains+Mono:wght@400;500;600&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="{{ asset('public/css/style.css') }}">
 </head>
-<h2>thi is</h2>
+
 <body data-cart-count="0">
 
 <header class="site-header">
