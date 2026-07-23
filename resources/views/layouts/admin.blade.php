4527f9562295be7907527cccfba2337fe05b27de
Abdul Haseeb <abdulhaseeb0085@gmail.com>
2026-07-23 16:53:32 +0500

    Updated some code

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -16,7 +16,7 @@
     
 
     <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet">
-    <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
+    <link href="{{ asset('public/css/styles.css') }}" rel="stylesheet">
 
     <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
 </head>
@@ -41,7 +41,7 @@
 
  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
 
-<script src="{{ asset('js/scripts.js') }}"></script>
+<script src="{{ asset('public/js/scripts.js') }}"></script>
 
 @yield('scripts')
 
